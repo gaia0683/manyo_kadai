17fd67d89abc8d3484f435b1372c9bdbb3a30ac7
bomute kim <kimbomte1996@icloud.com>
2022-09-07 14:14:07 +0900

    userが登録できるようnewとcreateアクションを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_06_050357) do
+ActiveRecord::Schema.define(version: 2022_09_07_023416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2022_09_06_050357) do
     t.date "end_time", default: -> { "CURRENT_DATE" }, null: false
     t.integer "status", default: 0, null: false
     t.integer "rank", default: 0, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
